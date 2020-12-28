d72697847e3359dfa1bbb6c7ebaf2980359164e8
manuel <ratel2379@outlook.com>
2020-12-28 12:32:54 +0100

    primer commit

---
+++ precache-manifest.05bd6d0b1849bba4940785f77a4410ad.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b8b638671e1ad3c05e9d2434757a814",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f264591fa86ac073c43c",
+    "url": "/static/css/main.5197f7f7.chunk.css"
+  },
+  {
+    "revision": "d9672b3b93b6047c0811",
+    "url": "/static/js/2.10202b99.chunk.js"
+  },
+  {
+    "revision": "a64acf8e3ad6aa1497056f0bb9a7ce4f",
+    "url": "/static/js/2.10202b99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f264591fa86ac073c43c",
+    "url": "/static/js/main.de3cace4.chunk.js"
+  },
+  {
+    "revision": "61514765329f8b6aa85a",
+    "url": "/static/js/runtime-main.9ce2ae71.js"
+  },
+  {
+    "revision": "4cded6d36ba81d9bccd22b55c3712a56",
+    "url": "/static/media/field-2023250.4cded6d3.png"
+  },
+  {
+    "revision": "8bd6f5904f8759e0862bbb0cb79be917",
+    "url": "/static/media/football-147854_1280.8bd6f590.png"
+  }
+]);
